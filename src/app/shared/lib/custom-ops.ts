6c795379321b273c54cf8a195195f4975db17d48
Jun Wei <kjunwei1996@gmail.com>
2020-10-21 13:38:31 +0800

    Auto-generated commit

--- src/app/shared/lib/custom-ops.ts
+++
@@ -1,9 +0,0 @@
-import { filter, throwIfEmpty } from 'rxjs/operators';
-import { pipe } from 'rxjs';
-
-export function throwIfFalse(predicate, error_func) {
-    return pipe(
-        filter(v => predicate(v)),
-        throwIfEmpty(error_func)
-    );
-}
